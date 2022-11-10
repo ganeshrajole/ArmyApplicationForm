929fc82739142e18f4ac68c85713cc0164452fe9
Ganesh Rajole <ganeshrajole@gmail.com>
2022-11-10 08:40:52 +0530

    ArmyApplication Form

---
+++ src/main/java/com/army/ArmyApplicationFormApplication.java
@@ -0,0 +1,13 @@
+package com.army;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmyApplicationFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmyApplicationFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/army/ArmyApplicationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.army;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmyApplicationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
